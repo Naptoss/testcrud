c608a5c2a1967061078a8d45cb1732056bc0c23c
Naptoss <tomgabinio@gmail.com>
2023-03-27 18:15:52 -0300

    Adicionado as condicões if e else

--- Demanda/variavel.py
+++ Demanda/variavel.py
@@ -2,5 +2,15 @@ a = 10
 b = 10
 
 soma = a + b
-
 print(soma)
+
+
+a = 3
+b = 5
+
+soma_x = a + b
+
+if soma_x == 8:
+    print("A soma do numero e igual a 8")
+else:
+    print("A soma e de outro valor")
